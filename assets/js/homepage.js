7218141998788e5af8916e6704afc56bf9eb448e
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-25 15:51:04 -0400

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,22 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -24,7 +40,17 @@ var getUserRepos = function(user) {
       alert("Unable to connect to GitHub");
     });
   };
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language")
+  if (language) {
+    getFeaturedRepos(language);
   
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
 var formSubmitHandler = function(event) {
   event.preventDefault();
 
@@ -94,3 +120,4 @@ var displayRepos = function(repos, searchTerm) {
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
